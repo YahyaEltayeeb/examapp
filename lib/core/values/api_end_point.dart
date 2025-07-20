36ff7bac1bb7b54e0075d1d2d25597c8c558e520
Yahya Mohamed <ym615318@gmail.com>
2025-07-20 18:13:43 +0300

    mergeLogIn

--- lib/core/values/api_end_point.dart
+++ lib/core/values/api_end_point.dart
@@ -1,7 +1,7 @@
 abstract class ApiEndPoint {
   static const String baseUrl = 'https://exam.elevateegy.com/api/v1/';
   static const String signUpEndPoint = 'auth/signup';
-  static const String forgetPasswordEndPoint='auth/forgotPassword';
-  static const String verifyResetCodeEndPoint='auth/verifyResetCode';
-  static const String resetPasswordEndPoint='auth/resetPassword';
- }
+  static const String forgetPAsswordEndPoint='auth/forgotPassword';
+  static const String verifyCode='verifyResetCode';
+  static const String resetPassword='resetPassword';
+}
